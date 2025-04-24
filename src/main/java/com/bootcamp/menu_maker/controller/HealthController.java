9eb37ea104d619dc4cdcd2663d93a223104bbffd
Víctor Ferré <victor522m@gmail.com>
2025-04-24 13:10:33 +0200

    Añadir healtContrller y max pool size = 2

---
+++ src/main/java/com/bootcamp/menu_maker/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.bootcamp.menu_maker.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Application is running!");
+    }
+}
+
